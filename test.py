0edc6e6d04bef0035b220ff1ecd678f7269a62ab
Pond-Man <61192404+Pond-Man@users.noreply.github.com>
2023-12-12 23:49:43 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,56 @@
 import torch
+from hcc2 import NeuralNetwork
+from gnt import GNT
+from PIL import Image
 from torchvision import transforms
-from hcc2 import NeuralNetwork, ChineseDataset
-import cv2
+from matplotlib import pyplot as plt
 
-model = NeuralNetwork().to('cuda')
-checkpoint = torch.load("./models/hccr-model2.0.pth")
-model.load_state_dict(checkpoint['model_state_dict'])
+# Define device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+model = NeuralNetwork()
+# Move model to device
+model = model.to(device)
+
+gnt = GNT()
+labels = gnt.create_label_dict("./data/gnt/train/")
+
+# Set model to evaluation mode
 model.eval()
 
+pic_file = 'C006-3.jpg'
 
-transforms = transforms.ToTensor()
+# Select a sample from the dataset
+image = Image.open(f'./data/gnt-jpg/train/{pic_file}')
+label = labels[pic_file]
+label = [ord(char) % 3755 for char in label]
 
-image_path = "./data/unit-tests/C002-0.jpg"
-image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-image = ChineseDataset.preprocess(image)
+transform = transforms.Compose([
+    transforms.ToTensor(),
+])
 
-input_data = torch.stack([transforms(image) for _ in range (64)]).to('cuda')
+# Apply the transformations, add a batch dimension, and move the image to the right device
+image = transform(image).unsqueeze(0).to(device)
 
+# Set the network to evaluation mode
+model.eval()
+
+# Pass the image through the network and get the prediction
 with torch.no_grad():
-    output = model(input_data)
+    prediction = model(image)
 
-_, predicted = torch.max(output.data, 1)
+_, predicted_label = torch.max(prediction, 1)
 
-print(f"Predicted: {predicted.tolist()}")
+# Move the image and label back to the CPU for visualization
+image = image.to('cpu')
 
+# Print the actual label
+print(f'Actual label: {label}')
+
+# Print the predicted label
+print(f'Predicted label: {predicted_label.item()}')
+
+# Display the image
+plt.imshow(image.squeeze(0).squeeze(0), cmap='gray')
+plt.show()
